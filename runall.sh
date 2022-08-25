ab463577c5387705317f67164a12f023c3d62406
Michael Schlottke-Lakemper <michael@sloede.com>
2022-08-25 15:23:47 +0200

    Add more manifests

--- runall.sh
+++ runall.sh
@@ -19,9 +19,14 @@ for i in 1 2 3; do
   # cd julia-1.8-M1.7 && ../ttfx.sh julia-1.8 --check-bounds=no && cd ..
   # echo
   # echo "^^^ These are the results for trial #$i in julia-1.8-M1.7/ ^^^"
-  # echo
-  # echo
-  cd julia-1.8-latest && ../ttfx.sh julia-1.8 --check-bounds=no && cd ..
   echo
-  echo "^^^ These are the results for trial #$i in julia-1.8-latest/ ^^^"
+  echo
+  cd julia-1.7-latest-check-bounds-no && ../ttfx.sh julia-1.7 --check-bounds=no && cd ..
+  echo
+  echo "^^^ These are the results for trial #$i in julia-1.7-latest-check-bounds-no/ ^^^"
+  echo
+  echo
+  cd julia-1.8-latest-check-bounds-no && ../ttfx.sh julia-1.8 --check-bounds=no && cd ..
+  echo
+  echo "^^^ These are the results for trial #$i in julia-1.8-latest-check-bounds-no/ ^^^"
 done
